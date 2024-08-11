1b93a9b103f348ef2744fc27394d4c80dea1cc9b
avdotcom <me+github@avilaca.com>
2024-08-11 17:37:32 -0400

    practicing adding bean and class

--- src/main/java/com/spring_in/learn_spring/App02HelloWorldSpring.java
+++ src/main/java/com/spring_in/learn_spring/App02HelloWorldSpring.java
@@ -8,5 +8,10 @@ public class App02HelloWorldSpring {
     public static void main (String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+    System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean(Address.class));
     }
 }
--- src/main/java/com/spring_in/learn_spring/HelloWorldConfiguration.java
+++ src/main/java/com/spring_in/learn_spring/HelloWorldConfiguration.java
@@ -3,12 +3,42 @@ package com.spring_in.learn_spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {};
+
+record Address(String firstLine, String secondLine, String city) {};
+
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
         return "Hello World!";
+    }
+
+    @Bean
+    public int age() {
+        return 15;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Jack", 18, new Address("Jack", "22B", "London"));
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("Jack", "22B", "London");
     }
 
 }
